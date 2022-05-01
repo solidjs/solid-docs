d73df720ef9759e6fc588f5711a50bc9a208366f
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2022-05-01 12:03:19 -0400

    Make the CodeTabs work, but suboptimally

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,5 +1,4 @@
-import { createSignal, For, JSX, useContext } from "solid-js";
-import { Dynamic } from "solid-js/web";
+import { createSignal, For, JSX, onMount, useContext } from "solid-js";
 import { Config, ConfigContext } from "./ConfigContext";
 
 type File = {
@@ -12,45 +11,67 @@ interface ICodeTabsProps {
   files: Record<Config["codeFormat"], File[]>;
 }
 
+/*
+  This component should be refactored to use the Dynamic and Show components
+  Once the hydration issues with those components are resolved. Right now,
+  this component eager-loads everything and simply hides the non-selected 
+  content using the CSS display property.
+*/
+
 export const CodeTabs = (props: ICodeTabsProps) => {
-  const [selected, setSelected] = createSignal<string>();
+  const [selected, setSelected] = createSignal(0);
   const [config, setConfig] = useContext(ConfigContext);
 
-  const availableTabs = () => props.files[config().codeFormat];
+  const jsxTabs = () => props.files.jsx;
+  const tsxTabs = () => props.files.tsx;
 
-  const selectedTab = () => {
-    const customSelected = availableTabs().find(
-      (file) => file.name === selected()
+  // Initial selected tab
+  onMount(() => {
+    const initialSelected = props.files[config().codeFormat].findIndex(
+      (el) => el.default
     );
-    if (customSelected) {
-      return customSelected;
-    }
-    const defaultSelected = availableTabs().find((file) => file.default);
-    if (defaultSelected) {
-      return defaultSelected;
-    }
-    return availableTabs()[0];
-  };
+    if (initialSelected) setSelected(initialSelected);
+  });
 
   return (
     <div class="my-10">
       <nav aria-label="Code example files" class="flex justify-between">
-        <div>
-          <For each={availableTabs()}>
-            {(file) => (
+        <div
+          style={{ display: config().codeFormat === "jsx" ? "block" : "none" }}
+        >
+          <For each={jsxTabs()}>
+            {(file, i) => (
               <button
                 classList={{
-                  "border-b-2 border-blue-400":
-                    selectedTab().name === file.name,
+                  "border-b-2 border-blue-400": i() === selected(),
                   "p-2": true,
                 }}
-                onClick={() => setSelected(file.name)}
+                onClick={() => setSelected(i())}
               >
                 {file.name}
               </button>
             )}
           </For>
         </div>
+
+        <div
+          style={{ display: config().codeFormat === "tsx" ? "block" : "none" }}
+        >
+          <For each={tsxTabs()}>
+            {(file, i) => (
+              <button
+                classList={{
+                  "border-b-2 border-blue-400": i() === selected(),
+                  "p-2": true,
+                }}
+                onClick={() => setSelected(i())}
+              >
+                {file.name}
+              </button>
+            )}
+          </For>
+        </div>
+
         <div class="flex gap-1">
           <button
             class={`py-2 px-3 rounded ${
@@ -71,7 +92,29 @@ export const CodeTabs = (props: ICodeTabsProps) => {
         </div>
       </nav>
       <div class="-mt-4">
-        <Dynamic component={selectedTab().component} />
+        <div
+          style={{ display: config().codeFormat === "jsx" ? "block" : "none" }}
+        >
+          <For each={jsxTabs()}>
+            {(file, i) => (
+              <div style={{ display: selected() === i() ? "block" : "none" }}>
+                {file.component()}
+              </div>
+            )}
+          </For>
+        </div>
+
+        <div
+          style={{ display: config().codeFormat === "tsx" ? "block" : "none" }}
+        >
+          <For each={tsxTabs()}>
+            {(file, i) => (
+              <div style={{ display: selected() === i() ? "block" : "none" }}>
+                {file.component()}
+              </div>
+            )}
+          </For>
+        </div>
       </div>
     </div>
   );

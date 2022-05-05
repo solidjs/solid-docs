1966824b784a064e16e5a990c5755c8d7b36046c
Dan Jutan <danjutan@gmail.com>
2022-05-05 00:57:52 -0400

    basic page ToC

--- src/components/nav/NavSection.tsx
+++ src/components/nav/NavSection.tsx
@@ -10,29 +10,10 @@ import {
 import { NavLink, useLocation } from "solid-app-router";
 
 import { usePageState } from "../PageStateContext";
+import { createEffect, createSignal } from "solid-js";
 
 function CollapsedIcon(props) {
-  return (
-    <div class={"duration-100 ease-in transition" + props.class}>▼</div>
-    // <svg
-    //   xmlns="http://www.w3.org/2000/svg"
-    //   width="20"
-    //   height="20"
-    //   viewBox="0 0 20 20"
-    //   class={"duration-100 ease-in transition" + props.class}
-    //   style="min-width: 20px; min-height: 20px;"
-    // >
-    //   <g fill="none" fill-rule="evenodd" transform="translate(-446 -398)">
-    //     <path
-    //       fill="currentColor"
-    //       fill-rule="nonzero"
-    //       d="M95.8838835,240.366117 C95.3957281,239.877961 94.6042719,239.877961 94.1161165,240.366117 C93.6279612,240.854272 93.6279612,241.645728 94.1161165,242.133883 L98.6161165,246.633883 C99.1042719,247.122039 99.8957281,247.122039 100.383883,246.633883 L104.883883,242.133883 C105.372039,241.645728 105.372039,240.854272 104.883883,240.366117 C104.395728,239.877961 103.604272,239.877961 103.116117,240.366117 L99.5,243.982233 L95.8838835,240.366117 Z"
-    //       transform="translate(356.5 164.5)"
-    //     ></path>
-    //     <polygon points="446 418 466 418 466 398 446 398"></polygon>
-    //   </g>
-    // </svg>
-  );
+  return <div class={"duration-100 ease-in transition" + props.class}>▼</div>;
 }
 
 export function NavGroup(props) {
@@ -81,16 +62,33 @@ function SectionPanel(props) {
 }
 
 export function NavItem(props) {
+  const isActive = () => props.href === useLocation().pathname;
+
+  const { sections } = usePageState();
+
   return (
-    <li>
+    <li class="">
       <NavLink
-        class="font-semibold text-base p-2 w-full rounded-none lg:rounded-r-lg text-left relative flex items-center justify-between pl-5"
+        class="font-semibold text-base p-2 pl-5 w-full rounded-none lg:rounded-r-lg text-left relative flex items-center justify-between"
         {...props}
         inactiveClass="hover:text-solid-light hover:dark:text-solid-darkdefault"
         activeClass="text-white bg-solid-light dark:bg-solid-light border-blue-40 active"
       >
         {props.children}
       </NavLink>
+      <Show when={isActive()}>
+        <div class="ml-4">
+          <ol class="pl-5 mt-2 list-decimal space-y-1">
+            <For each={sections()}>
+              {(item) => (
+                <li class="text-base">
+                  <a href={"#" + item.href}>{item.title}</a>
+                </li>
+              )}
+            </For>
+          </ol>
+        </div>
+      </Show>
     </li>
   );
 }

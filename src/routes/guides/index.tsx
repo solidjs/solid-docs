8e2acfb4140df8383960490925e15e3a9f0116f2
Dan Jutan <danjutan@gmail.com>
2022-05-08 21:15:44 -0400

    root guides redirect

---
+++ src/routes/guides/index.tsx
@@ -0,0 +1,9 @@
+import { Navigate } from "solid-app-router";
+
+export default function () {
+  return (
+    <>
+      <Navigate href="./getting-started-with-solid/welcome"></Navigate>
+    </>
+  );
+}

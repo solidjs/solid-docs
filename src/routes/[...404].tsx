b14e48156b3ec3dd0297fc1d8e2e7de99ac57c01
Dan Jutan <danjutan@gmail.com>
2022-05-08 20:27:13 -0400

    dummy 404

---
+++ src/routes/[...404].tsx
@@ -0,0 +1,8 @@
+export default function () {
+  return (
+    <div class="w-full mt-12 flex flex-col justify-center items-center">
+      <div class="font-bold text-xl">404</div>
+      <div class="font-bold text-xl">This page doesn't quite exist yet</div>
+    </div>
+  );
+}

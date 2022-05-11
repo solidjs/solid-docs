af227fefc42b9d2bacf5953fba8d8de895d5d05c
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2022-05-11 11:45:01 -0400

    Fix redirect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 import { Navigate } from "solid-app-router";
 
 export default function Layout() {
-  return <Navigate href="/guides" />;
+  return (
+    <Navigate href="/guides/getting-started-with-solid/welcome"></Navigate>
+  );
 }

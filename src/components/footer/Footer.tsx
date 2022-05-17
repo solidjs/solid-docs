45a8191f89fb31598a9e2c065516dba29be37b80
Dan Jutan <danjutan@gmail.com>
2022-05-17 17:03:38 -0400

    edit button

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,21 @@
 import { HelpButton } from "./HelpButton";
+import IconEdit from "~icons/heroicons-outline/pencil-alt";
+import { useLocation } from "solid-app-router";
+import { createEffect } from "solid-js";
 
 export default function Footer() {
+  const { pathname } = useLocation();
+
+  const url = () =>
+    `https://github.com/solidjs/solid-docs-next/edit/main/src/routes${pathname}.mdx`;
+
   return (
-    <footer class="flex justify-center pb-8 border-t-1 border-dotted pt-10">
+    <footer class="flex flex-col sm:flex-row justify-between pb-8 border-t-1 border-dotted pt-10">
       <HelpButton />
-      <a class="flex w-1/2"></a>
+
+      <a class="flex gap-2 hover:underline" href={url()}>
+        <IconEdit></IconEdit> Edit this page
+      </a>
     </footer>
   );
 }
--- src/components/footer/HelpButton.tsx
+++ src/components/footer/HelpButton.tsx
@@ -3,7 +3,7 @@ import IconDiscord from "~icons/ic/baseline-discord";
 export const HelpButton = () => {
   return (
     <a
-      class="flex gap-2 w-1/2 hover:underline"
+      class="flex gap-2 hover:underline"
       href="https://discord.com/invite/solidjs"
     >
       <IconDiscord /> Need help? Don't hesitate to ask us on Discord!

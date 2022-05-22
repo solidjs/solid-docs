86d8057678db6ced625b6c0e74b830259bedb044
Dan Jutan <danjutan@gmail.com>
2022-05-22 10:42:09 -0400

    index pages

--- src/routes/guides/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "solid-app-router";
-
-export default function () {
-  return (
-    <>
-      <Navigate href="./getting-started-with-solid/welcome"></Navigate>
-    </>
-  );
-}

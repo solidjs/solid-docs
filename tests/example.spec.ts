4a1cbb2e94841ecd953b77fec7a07ec4fea22bc6
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2022-05-26 23:51:52 -0400

    Bump CI node version, add a11y checks

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,10 @@
 import { test, expect } from "@playwright/test";
+import { checkA11y, injectAxe } from "axe-playwright";
 
 test("basic test", async ({ page }) => {
   await page.goto("http://localhost:3000");
+  await injectAxe(page);
+  await checkA11y(page);
   const main = page.locator("article");
   await expect(main).toHaveText(/^Welcome/);
 });

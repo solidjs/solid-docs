de54f3be4f7fc034da5f3a6d9a67d4b64d97d93a
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2022-05-26 22:23:27 -0400

    Add contributors to front page

---
+++ src/components/contributors/Contributors.tsx
@@ -0,0 +1,48 @@
+import { createResource, For, Show } from "solid-js";
+
+type GithubUser = Record<"avatar_url" | "html_url" | "login", string>;
+
+export const Contributors = () => {
+  const [contributors] = createResource<GithubUser[]>(async () => {
+    const res = await fetch(
+      "https://api.github.com/repos/solidjs/solid-docs-next/contributors"
+    );
+    return shuffle(await res.json());
+  });
+
+  return (
+    <div class="flex gap-3 mb-10">
+      <Show when={!!contributors()}>
+        <For each={contributors()}>
+          {(contributor) => (
+            <a
+              href={contributor.html_url}
+              class="hover:opacity-70 ease-in duration-200"
+            >
+              <img
+                alt={`GitHub user ${contributor.login}`}
+                src={`${contributor.avatar_url}&s=48`}
+                class="rounded-full w-15"
+              />
+            </a>
+          )}
+        </For>
+      </Show>
+    </div>
+  );
+};
+
+function shuffle(array: any[]) {
+  const copied = [...array];
+  let currentIndex = copied.length,
+    randomIndex;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [copied[currentIndex], copied[randomIndex]] = [
+      copied[randomIndex],
+      copied[currentIndex],
+    ];
+  }
+  return copied;
+}

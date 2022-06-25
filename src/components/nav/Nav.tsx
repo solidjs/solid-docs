d9a191cc3394c6de447b1cf328a994ceee321453
Dan Jutan <danjutan@gmail.com>
2022-06-25 20:57:39 +0000

    overflw-y-auto

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -16,7 +16,7 @@ export default function Nav(props: { docsMode: "start" | "regular" }) {
   });
 
   return (
-    <div class="lg:max-h-screen lg:sticky lg:top-0 no-bg-scrollbar py-0 lg:max-w-xs w-full shadow lg:shadow-none z-50 overflow-y-scroll">
+    <div class="lg:max-h-screen lg:sticky lg:top-0 no-bg-scrollbar py-0 lg:max-w-xs w-full shadow lg:shadow-none z-50 overflow-y-auto">
       <NavHeader
         docsMode={props.docsMode}
         showMenu={showMenu()}

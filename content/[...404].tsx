8a6398dc908b670bdd00a3051778a48c4f7390f3
Nikhil Saraf <nsaraf98@gmail.com>
2022-08-01 01:14:35 -0700

    fix status code

--- content/[...404].tsx
+++ content/[...404].tsx
@@ -1,11 +1,11 @@
-import { StatusCode } from "solid-start/server";
+import { HttpStatusCode } from "solid-start/server";
 
 export default function () {
   return (
     <div class="h-full mt-12 flex flex-col justify-center items-center text-center">
       <div class="font-bold text-xl">404</div>
       <div class="font-bold text-xl">This page doesn't quite exist yet</div>
-      <StatusCode code={404} />
+      <HttpStatusCode code={404} />
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -118,6 +118,7 @@ export default defineConfig({
   ],
   optimizeDeps: {
     exclude: ["solid-headless"],
+    include: ["debug"],
   },
   ssr: {
     noExternal: ["solid-headless", "solid-heroicons"],

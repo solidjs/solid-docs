a6b1b62306cc4a14506a6a61078001165bbc6ce7
robin marillia <robin4a4@gmail.com>
2022-08-15 17:19:37 +0200

    fix gear icon color in light mode

--- src/components/nav/NavPreferences.tsx
+++ src/components/nav/NavPreferences.tsx
@@ -7,7 +7,7 @@ export function NavPreferences() {
     <details class="border border-solid-lightitem dark:border-solid-darkitem rounded-lg">
       <summary class="flex items-center justify-between p-4 w-full">
         <div class="flex items-center gap-2 font-semibold">
-          <div class="bg-solid-darkitem rounded-lg p-1">
+          <div class="bg-solid-lightitem dark:bg-solid-darkitem rounded-lg p-1">
             <IconGear class="w-4 h-4" />
           </div>
           Preferences

01d671ed1327d889d1c93d650bc5914b345eada0
Michael Essiet <essiet2002@gmail.com>
2023-01-11 23:04:13 +0100

    improved the summary section on mobile and desktop a bit

--- src/components/Dot.tsx
+++ src/components/Dot.tsx
@@ -1,7 +1,7 @@
 export default function Dot(props: { number: number }) {
   return (
-    <div class="rounded-full pt-1 w-6 h-6 border border-solid-lightitem dark:border-solid-darkitem grid place-content-center bg-solid-light dark:bg-white text-solid-accent font-bold">
-      {props.number}
+    <div class="rounded-full pt-1 min-w-6 w-6 h-6 border border-solid-lightitem dark:border-solid-darkitem grid place-content-center bg-solid-light dark:bg-white text-solid-accent font-bold">
+      <span>{props.number}</span>
     </div>
   );
 }

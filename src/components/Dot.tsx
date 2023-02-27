1f0c9c810e95b306010493eb3ed493ea18f8503b
Evelyne Lachance <eslachance@gmail.com>
2023-02-27 05:33:55 -0500

    Remove odd top padding on number dot

--- src/components/Dot.tsx
+++ src/components/Dot.tsx
@@ -1,6 +1,6 @@
 export default function Dot(props: { number: number }) {
   return (
-    <div class="rounded-full pt-1 min-w-6 w-6 h-6 border border-solid-lightitem dark:border-solid-darkitem grid place-content-center bg-solid-light dark:bg-white text-solid-accent font-bold">
+    <div class="rounded-full min-w-6 w-6 h-6 border border-solid-lightitem dark:border-solid-darkitem grid place-content-center bg-solid-light dark:bg-white text-solid-accent font-bold">
       <span>{props.number}</span>
     </div>
   );

756e8978abf8933b55f6cf2d958c80b2c70e2ad7
Sarah Gerrard <gerrardsarah@gmail.com>
2023-07-25 22:51:48 +0000

    changed some components to reduce files

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,18 @@
-import { HelpButton } from "./HelpButton";
 import IconEdit from "~icons/heroicons-outline/pencil-alt";
 import { useLocation } from "@solidjs/router";
+import IconDiscord from "~icons/ic/baseline-discord";
+
+export const HelpButton = () => {
+  return (
+    <a
+      class="flex items-center gap-2 hover:underline"
+      href="https://discord.com/invite/solidjs"
+    >
+      <IconDiscord /> Need help? Don't hesitate to ask us on Discord!
+    </a>
+  );
+};
+
 
 export default function Footer() {
   const location = useLocation();

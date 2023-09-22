2993deebe9c4b977f741855396078f88f55abfa7
Stu Kabakoff <stutrek@gmail.com>
2023-09-22 11:20:53 -0400

    add proper Open in Eraser link

--- src/components/EraserLink.tsx
+++ src/components/EraserLink.tsx
@@ -62,8 +62,13 @@ const EraserLink = ({
 		<div class="relative">
 			<Link href={linkUrl}>
 				<img src={imageUrl} alt={""} />
+				<img
+					src="https://firebasestorage.googleapis.com/v0/b/second-petal-295822.appspot.com/o/images%2Fgithub%2FOpen%20in%20Eraser.svg?alt=media&token=968381c8-a7e7-472a-8ed6-4a6626da5501
+			"
+					class="absolute top-4 right-4"
+					alt="Open in Eraser"
+				/>
 			</Link>
-			<div class="absolute top-1 right-2">Eraser</div>
 		</div>
 	)
 }

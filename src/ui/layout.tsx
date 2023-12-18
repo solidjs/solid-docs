56c73da79fe0d0773d552b0631553e61b062b1a8
JasonHassold <JasonHassold@gmail.com>
2023-12-18 03:18:33 -0500

    custom scrollbar changes

--- src/ui/layout.tsx
+++ src/ui/layout.tsx
@@ -22,7 +22,7 @@ export const Layout: ParentComponent = (props) => {
 					<div class="absolute inset-y-0 right-0 w-[50vw] bg-slate-50 dark:hidden" />
 					<div class="absolute bottom-0 right-0 top-16 hidden h-12 w-px bg-gradient-to-t from-slate-800 dark:block" />
 					<div class="absolute bottom-0 right-0 top-28 hidden w-px bg-slate-800 dark:block" />
-					<div class="sticky top-[4.75rem]  h-[calc(100vh-4.75rem)] w-64 overflow-y-auto overflow-x-hidden py-16 pl-1 pr-8 xl:w-72 xl:pr-16">
+					<div class="sticky top-[4.75rem] h-[calc(100vh-4.75rem)] w-64 overflow-y-auto overflow-x-hidden py-16 pl-1 pr-8 xl:w-72 xl:pr-16 custom-scrollbar">
 						<MainNavigation />
 					</div>
 				</div>

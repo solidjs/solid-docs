c31148b3067f49a99b16c6bea92c12e04d7ff4b2
Lucas Bogerd <l.bogerd@protonmail.com>
2023-12-26 13:40:44 +0100

    Update BasicBookshelf.tsx
    Change name to the correct "Sara" instead of "Sarah". See: https://www.saranahmed.com or https://en.wikipedia.org/wiki/Sara_Ahmed

--- content/guides/tutorials/getting-started-with-solid/components/BasicBookshelf.tsx
+++ content/guides/tutorials/getting-started-with-solid/components/BasicBookshelf.tsx
@@ -10,7 +10,7 @@ type Book = {
 const initialBooks: Book[] = [
   { title: "Code Complete", author: "Steve McConnell" },
   { title: "The Hobbit", author: "J.R.R. Tolkien" },
-  { title: "Living a Feminist Life", author: "Sarah Ahmed" },
+  { title: "Living a Feminist Life", author: "Sara Ahmed" },
 ];
 
 interface IBookshelfProps {

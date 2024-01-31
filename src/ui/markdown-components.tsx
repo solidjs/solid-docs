01539f23c3cc0286285357556a4c1cd94665495a
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2024-01-31 22:19:10 +0000

    Make only non-relative URLs open in a new tab

--- src/ui/markdown-components.tsx
+++ src/ui/markdown-components.tsx
@@ -114,7 +114,9 @@ export default {
 		const [, rest] = splitProps(props, ["children"]);
 		const resolved = children(() => props.children);
 		const resolvedArray = resolved.toArray();
-
+		let extraAttrs = {};
+		if (props.href.startsWith("https://") || props.href.startsWith("http://"))
+			extraAttrs = { target: "_blank", rel: "noopener noreferrer" };
 		if (
 			// Server side
 			(isServer &&
@@ -131,8 +133,7 @@ export default {
 			return (
 				<A
 					class="[&>code]:shadow-[0_0_0_1px_#38bdf8] hover:[&>code]:shadow-[0_0_0_2px_#38bdf8]"
-					target="_blank"
-					rel="noopener noreferrer"
+					{...extraAttrs}
 					{...rest}
 				>
 					{resolved()}
@@ -143,8 +144,7 @@ export default {
 				<A
 					{...rest}
 					class={`no-underline shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#38bdf8),inset_0_calc(-1*(var(--tw-prose-underline-size,2px)+2px))_0_0_var(--tw-prose-underline,theme(colors.sky.400))] hover:[--tw-prose-underline-size:4px] dark:[--tw-prose-background:theme(colors.slate.900)] dark:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.sky.800))] dark:hover:[--tw-prose-underline-size:6px] dark:text-sky-400 text-sky-700 font-semibold`}
-					target="_blank"
-					rel="noopener noreferrer"
+					{...extraAttrs}
 				>
 					{resolved()}
 				</A>

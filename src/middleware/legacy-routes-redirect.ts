128143697b175da28a923b5d03834d3e9c28b378
Atila Fassina <atila@fassina.eu>
2024-02-14 15:53:02 +0100

    use object dictionary instead

--- src/middleware/legacy-routes-redirect.ts
+++ src/middleware/legacy-routes-redirect.ts
@@ -2,26 +2,22 @@ import { redirect } from "@solidjs/router";
 import { type FetchEvent } from "@solidjs/start/server/types";
 
 /**
- * Redirect Map
- * [origin, destination]
+ * Redirect Dictionary
+ * {origin: destination}
  */
-const LEGACY_ROUTES = new Map([
-	[
-		"/references/api-reference/control-flow/Suspense",
+const LEGACY_ROUTES = {
+	"/references/api-reference/control-flow/Suspense":
 		"/reference/components/suspense",
-	],
-] as const);
+} as const;
 
-function isLegacyRoute(
-	path: string
-): path is Parameters<(typeof LEGACY_ROUTES)["get"]>[number] {
-	return LEGACY_ROUTES.has(path);
+function isLegacyRoute(path: string): path is keyof typeof LEGACY_ROUTES {
+	return path in LEGACY_ROUTES;
 }
 
 export const handleLegacyRoutes = (event: FetchEvent) => {
 	const { pathname } = new URL(event.request.url);
 
 	if (isLegacyRoute(pathname)) {
-		return redirect(LEGACY_ROUTES.get(pathname)!, 308);
+		return redirect(LEGACY_ROUTES[pathname], 308);
 	}
 };

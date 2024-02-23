2f42660809fe37aec76da8611de2365e66562dcc
doeixd <doeixd@gmail.com>
2024-02-23 00:39:52 -0500

    adjusted main nav tab spacing

--- src/ui/layout/main-navigation.tsx
+++ src/ui/layout/main-navigation.tsx
@@ -41,12 +41,12 @@ function DirList(props: { list: Entry[] }) {
 				if (Array.isArray(item.children)) {
 					return (
 						<li>
-							<span class="font-display font-semibold text-slate-700 dark:text-slate-100 lg:text-sm">
+							<span class="font-display font-semibold tracking-wide text-slate-700 dark:text-slate-100 lg:text-sm">
 								{item.title}
 							</span>
 							<ul
 								role="list"
-								class="ml-2 mt-2 space-y-2 border-l-2 border-slate-400 dark:border-slate-700 lg:mt-4 lg:space-y-4 lg:border-slate-400"
+								class="ml-2 mt-2 space-y-2 border-l-[1px] border-slate-300 dark:border-slate-700 lg:mt-4 lg:space-y-4 lg:border-slate-300"
 							>
 								<For each={item.children}>
 									{(child) =>
@@ -55,7 +55,7 @@ function DirList(props: { list: Entry[] }) {
 												<li>
 													<Collapsible.Root defaultOpen={true}>
 														<Collapsible.Trigger class="relative flex justify-between hover:cursor-pointer w-full pl-3.5 dark:text-slate-300 dark:text-slate-300 lg:text-sm group">
-															<span class="font-display font-semibold dark:text-slate-100 lg:text-sm">
+															<span class="font-display font-semibold dark:text-slate-100 lg:text-sm tracking-wide">
 																{child.title}
 															</span>
 															<Icon
@@ -66,7 +66,7 @@ function DirList(props: { list: Entry[] }) {
 														<Collapsible.Content class="navigation_collapsible">
 															<ul
 																role="list"
-																class="ml-4 mt-2 space-y-2 border-l-2 border-slate-400 dark:border-slate-700 lg:mt-4 lg:space-y-3 dark:lg:border-slate-700 text-base lg:text-sm"
+																class="ml-4 mt-2 space-y-2 border-l-[1px] border-slate-300 dark:border-slate-700 lg:mt-4 lg:space-y-3 dark:lg:border-slate-700 text-base lg:text-sm"
 															>
 																<DirList list={child.children} />
 															</ul>
@@ -101,22 +101,22 @@ export function MainNavigation() {
 	return (
 		<nav class="overflow-y-auto custom-scrollbar h-full md:h-[calc(100vh-7rem)] pb-20">
 			<Tabs.Root defaultValue={isReference() ? "reference" : "learn"}>
-				<Tabs.List class="sticky top-0 flex w-full pb-4 pr-4 z-10 md:dark:bg-slate-900 md:bg-slate-50">
+				<Tabs.List class="sticky top-0 flex w-full z-10 md:dark:bg-slate-900 md:bg-slate-50 border-b-[1px] border-blue-150 dark:border-slate-800 ">
 					<Tabs.Trigger
 						value="learn"
-						class="inline-block flex-1 ml-2 px-6 py-2 outline-none hover:bg-blue-500/30 dark:hover:bg-blue-300/20  dark:focus-visible:bg-blue-800 dark:text-slate-100 hover:font-bold"
+						class="inline-block flex-1 px-4 tracking-wider py-5 outline-none hover:bg-blue-500/10 dark:hover:bg-blue-300/20  dark:focus-visible:bg-blue-800 dark:text-slate-100 hover:font-semibold"
 					>
 						Learn
 					</Tabs.Trigger>
 					<Tabs.Trigger
 						value="reference"
-						class="inline-block flex-1 px-6 py-2 hover:bg-blue-500/30 dark:hover:bg-blue-300/20  dark:focus-visible:bg-blue-800 dark:text-slate-100 hover:font-bold"
+						class="inline-block flex-1 px-4 tracking-wider py-5 outline-none hover:bg-blue-500/10 dark:hover:bg-blue-300/20  dark:focus-visible:bg-blue-800 dark:text-slate-100 hover:font-semibold"
 					>
 						Reference
 					</Tabs.Trigger>
-					<Tabs.Indicator class="absolute bottom-4 bg-blue-500 dark:bg-blue-500 transition-all duration-250 h-[2px]" />
+					<Tabs.Indicator class="absolute bottom-0 bg-blue-500 dark:bg-blue-500 transition-all duration-250 h-[2px]" />
 				</Tabs.List>
-				<Tabs.Content value="learn" class="w-full relative mt-8 text-base">
+				<Tabs.Content value="learn" class="w-full relative mt-8 text-base prose-a:tracking-[0.2px]">
 					<Show when={learn()} fallback={<p>No routes found...</p>}>
 						{(learnList) => (
 							<ul role="list" class="space-y-6 px-4">

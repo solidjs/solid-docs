dbb048a0b85d6c85173067cacfe135f4906419fa
Tito Bouzout <tito.bouzout@gmail.com>
2024-03-06 06:52:32 -0300

    use custom scrollbars only on windows and when is not firefox

--- public/scripts/browser-specific.js
+++ public/scripts/browser-specific.js
@@ -1,3 +1,6 @@
 // add class to `html` for browser specific styles, such scrollbars
 if (navigator.userAgent.includes("Firefox"))
 	document.documentElement.classList.add("firefox");
+// add class to `html` for platform specific styles, such scrollbars
+if (navigator.userAgent.includes("Windows"))
+	document.documentElement.classList.add("windows");

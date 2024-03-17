9301af790f10d821d3138f3b72e2892f0fdc4cd9
Daniel Afonso <danieljcafonso@gmail.com>
2024-03-17 18:45:57 +0000

    feat: add pagination items to dictionary

--- src/ui/pagination.tsx
+++ src/ui/pagination.tsx
@@ -1,6 +1,7 @@
 import { A } from "~/ui/i18n-anchor";
-import { Show } from "solid-js";
+import { Show, Suspense } from "solid-js";
 import flatEntries from "solid:collection/entries";
+import { useI18n } from "~/i18n/i18n-context";
 
 /**
  * temporary until we have proper types inside collections
@@ -29,38 +30,44 @@ export function Pagination(props: Pagination) {
 		}
 	};
 
+	const i18n = useI18n();
+
 	return (
-		<nav class="flex justify-between mt-10 pt-6 border-t border-slate-200 dark:border-slate-800">
-			<Show when={previous()}>
-				{(entry) => (
-					<div>
-						<span class="font-semibold text-slate-900 dark:text-white">
-							Previous
-						</span>
-						<A
-							class="flex items-center gap-x-1 text-base font-medium text-slate-500 hover:text-blue-600 dark:text-slate-300 dark:hover:text-blue-300 flex-row-reverse no-underline"
-							href={entry().path}
-						>
-							← {entry().title}
-						</A>
-					</div>
-				)}
+		<Suspense>
+			<Show when={i18n.t}>
+				<nav class="flex justify-between mt-10 pt-6 border-t border-slate-200 dark:border-slate-800">
+					<Show when={previous()}>
+						{(entry) => (
+							<div>
+								<span class="font-display text-sm font-medium text-slate-900 dark:text-white">
+									{i18n.t("pagination.previous")}
+								</span>
+								<A
+									class="flex items-center gap-x-1 text-base font-medium text-slate-500 hover:text-blue-600 dark:text-slate-300 dark:hover:text-blue-300 flex-row-reverse no-underline"
+									href={entry().path}
+								>
+									← {entry().title}
+								</A>
+							</div>
+						)}
+					</Show>
+					<Show when={next()}>
+						{(entry) => (
+							<div>
+								<span class="font-display text-sm font-medium text-slate-900 dark:text-white">
+									{i18n.t("pagination.next")}
+								</span>
+								<A
+									class="flex items-center gap-x-1 text-base font-medium text-slate-500 hover:text-blue-700 dark:text-slate-300 dark:hover:text-blue-300 flex-row-reverse no-underline"
+									href={entry().path}
+								>
+									{entry().title} →
+								</A>
+							</div>
+						)}
+					</Show>
+				</nav>
 			</Show>
-			<Show when={next()}>
-				{(entry) => (
-					<div>
-						<span class="font-semibold text-slate-900 dark:text-white">
-							Next
-						</span>
-						<A
-							class="flex items-center gap-x-1 text-base font-medium text-slate-500 hover:text-blue-700 dark:text-slate-300 dark:hover:text-blue-300 flex-row-reverse no-underline"
-							href={entry().path}
-						>
-							{entry().title} →
-						</A>
-					</div>
-				)}
-			</Show>
-		</nav>
+		</Suspense>
 	);
 }

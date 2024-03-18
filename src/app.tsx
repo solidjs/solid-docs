7d3bdc237e74f3d228460641e62bc02f618396f7
Atila Fassina <atila@fassina.eu>
2024-03-18 16:11:08 +0100

    Update to latest SolidStart (#594)

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,15 @@
 // @refresh reload
 import { Router } from "@solidjs/router";
-import { A } from "~/ui/i18n-anchor";
-import { FileRoutes, HttpStatusCode } from "@solidjs/start";
+import { FileRoutes } from "@solidjs/start/router";
 import { MDXProvider } from "solid-mdx";
 import { ErrorBoundary, Suspense, createEffect } from "solid-js";
 import { MetaProvider, Title } from "@solidjs/meta";
 import Md from "~/ui/markdown-components";
 import { Layout } from "~/ui/layout";
-import "~/styles.css";
 import { ThemeProvider, useThemeContext } from "./data/theme-provider";
 import { I18nProvider } from "@kobalte/core";
 import { NotFound } from "./ui/not-found";
+import "~/styles.css";
 
 export default function App() {
 	return (

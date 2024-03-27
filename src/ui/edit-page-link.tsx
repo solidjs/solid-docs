69e1cf8ba43ec31e0e664f08c64070f95f75ae43
Birk Skyum <birk.skyum@pm.me>
2024-03-27 13:47:55 +0100

    Edit page link on project root pages

--- src/ui/edit-page-link.tsx
+++ src/ui/edit-page-link.tsx
@@ -7,7 +7,22 @@ import { useI18n } from "~/i18n/i18n-context";
 
 export const EditPageLink: Component = () => {
 	const i18n = useI18n();
-	const currentPath = createMemo(()=>useLocation().pathname);
+	const currentPath = createMemo(() => {
+		const pathname = useLocation().pathname;
+
+		switch (pathname) {
+			case "/":
+				return "/index";
+			case "/solid-router":
+			case "/solid-router/":
+				return "/solid-router/index";
+			case "/solid-start":
+			case "/solid-start/":
+				return "/solid-start/index";
+			default:
+				return pathname;
+		}
+	});
 
 	const srcPath = createMemo(() => {
 		return `https://github.com/solidjs/solid-docs-next/edit/main/src/routes${getEntryFileName(currentPath())}.mdx`.replace(
@@ -19,6 +34,7 @@ export const EditPageLink: Component = () => {
 		<a
 			class="flex no-underline hover:font-bold hover:text-blue-700 dark:hover:text-blue-300 dark:text-slate-300 "
 			href={srcPath()}
+			target="_blank"
 		>
 			<Icon class="mr-1 w-[16px]" path={pencilSquare} />
 			{i18n.t("contribute.edit")}

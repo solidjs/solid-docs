a58d967ea8229f2bfe5e8f1e18c94794eed5a657
Daniel Afonso <danieljcafonso@gmail.com>
2024-04-16 22:21:34 +0100

    fix: theme toggler

--- src/ui/layout/theme-selector.tsx
+++ src/ui/layout/theme-selector.tsx
@@ -1,4 +1,4 @@
-import { Component, For } from "solid-js";
+import { Component, For, Show } from "solid-js";
 import { DropdownMenu } from "@kobalte/core";
 import { Icon } from "solid-heroicons";
 import { useThemeContext } from "~/data/theme-provider";
@@ -10,10 +10,16 @@ export const ThemeSelector: Component = () => {
 	return (
 		<DropdownMenu.Root gutter={10}>
 			<DropdownMenu.Trigger class="flex h-6 w-6 items-center justify-center rounded-lg shadow-md shadow-black/5 ring-1 ring-black/10 dark:bg-slate-800 dark:ring-inset dark:ring-white/60 shrink-0">
-				<Icon
-					class="w-4 h-4 fill-slate-700 dark:fill-slate-200"
-					path={selectedTheme()!.icon}
-				/>
+				<Show when={selectedTheme()} fallback={<></>} keyed>
+					{(theme) => {
+						return (
+							<Icon
+								class="w-4 h-4 fill-slate-700 dark:fill-slate-200"
+								path={theme.icon}
+							/>
+						);
+					}}
+				</Show>
 			</DropdownMenu.Trigger>
 			<DropdownMenu.Portal>
 				<DropdownMenu.Content class="z-50 w-36 space-y-1 rounded-xl bg-white p-2 text-sm shadow-md shadow-black/5 ring-1 ring-black/5 dark:bg-slate-800 dark:ring-white/5">

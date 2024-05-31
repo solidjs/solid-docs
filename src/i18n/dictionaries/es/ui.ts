e19eccbda37bc4ed4ae02958bfc6f128b3a6b22b
Roy Eden <roy.ede@gmail.com>
2024-05-31 20:32:32 -0300

    Create `es/ui.ts`

---
+++ src/i18n/dictionaries/es/ui.ts
@@ -0,0 +1,47 @@
+export default {
+	// hero
+	"hero.title": "UIs sin esfuerzo con precisión reactiva.",
+	"hero.subtitle": "SolidJS es un moderno framework de Javascript para la web actual.",
+	"hero.button.primary": "Comenzar ahora",
+	"hero.button.secondary": "Unirse a la comunidad",
+
+	// main navigation
+	"main.nav.tab.learn": "Aprenda",
+	"main.nav.tab.reference": "Referencia",
+	"main.nav.section.concepts": "Conceptos",
+	"main.nav.section.components": "Componentes",
+	"main.nav.section.control.flow": "Flujo de control",
+	"main.nav.section.derived.values": "Valores derivados",
+	"main.nav.section.routing": "Ruteo",
+	"main.nav.section.advanced.concepts": "Conceptos avanzados",
+	"main.nav.section.guides": "Guías",
+	"main.nav.section.configuration": "Configuración",
+	"main.nav.section.basic.reactivity": "Reactividad básica",
+	"main.nav.section.component.apis": "APIs de componentes",
+	"main.nav.section.jsx.attributes": "Atributos de JSX",
+	"main.nav.section.lifecycle": "Ciclos de vida",
+	"main.nav.section.reactive.utilities": "Utilidades reactivas",
+	"main.nav.section.rendering": "Renderizado",
+	"main.nav.section.secondary.primitives": "Primitivas secundarias",
+	"main.nav.section.stores": "Stores",
+	"main.nav.section.solid.router": "Solid-Router",
+	"main.nav.section.solid.router.components": "Componentes",
+	"main.nav.section.solid.router.data.apis": "APIs de datos",
+	"main.nav.section.solid.router.load.functions": "Funciones de carga",
+	"main.nav.section.solid.router.primitives": "Primitivas",
+	"main.nav.no.routes": "No se encontró la ruta buscada",
+
+	// ToC
+	"toc.this.page": "En esta página",
+	"toc.overview": "Sumario",
+	"contribute.title": "Contribuya",
+	"contribute.edit": "Editar esta página",
+	"contribute.report": "Reportar un error en esta página",
+
+	// pagination
+	"pagination.next": "Siguiente",
+	"pagination.previous": "Anterior",
+
+	// 404
+	"missing.translation": "Esta sección todavía no fue traducida",
+} as const;

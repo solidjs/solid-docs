a650440ab54f014935818ab17a27978bf593b8da
Roy Eden <roy.ede@gmail.com>
2024-05-31 20:36:47 -0300

    Add spanish into `dictionaries/index.ts`

--- src/i18n/dictionaries/index.ts
+++ src/i18n/dictionaries/index.ts
@@ -1,12 +1,15 @@
 import english from "./en/ui";
 import ptbr from "./pt-br/ui";
+import es from "./es/ui";
 
 export const dictionaries = {
 	default: english,
 	// "pt-br": ptbr,
+	// es,
 };
 
 export const languages: { [key: string]: string } = {
 	en: "English",
 	// "pt-br": "Português do Brasil",
+	// es: "Español",
 };

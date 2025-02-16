49717c7ac4d17aee406f490e86f8f1938044c336
Robert Martin <rmartin@rmart.in>
2025-02-16 06:35:58 +0000

    docs: resolve formatting issue in page-issue-link

--- src/ui/page-issue-link.tsx
+++ src/ui/page-issue-link.tsx
@@ -8,14 +8,16 @@ export const PageIssueLink: Component = () => {
 	const i18n = useI18n();
 
 	const srcPath = createMemo(() => {
-		return `https://github.com/solidjs/solid-docs-next/issues/new` +
-			`?assignees=ladybluenotes` +
-			`&labels=improve+documentation%2Cpending+review` +
-			`&projects=` +
-			`&template=CONTENT.yml` +
-			`&title=[Content]:` +
-			`&subject=${getEntryFileName()}` +
-			`&page=${window.location.href}`
+		return (
+			"https://github.com/solidjs/solid-docs-next/issues/new" +
+			"?assignees=ladybluenotes" +
+			"&labels=improve+documentation%2Cpending+review" +
+			"&projects=" +
+			"&template=CONTENT.yml" +
+			"&title=[Content]:" +
+			"&subject=${getEntryFileName()}" +
+			"&page=${window.location.href}"
+		);
 	});
 	return (
 		<a

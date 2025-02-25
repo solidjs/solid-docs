0a58675d0c06feecbd686f50341cd727d1ed7822
Brendan Allan <brendonovich@outlook.com>
2025-02-26 03:41:36 +0800

    fix themes

--- src/solidbase-theme/Layout.tsx
+++ src/solidbase-theme/Layout.tsx
@@ -5,8 +5,11 @@ import { Layout } from "~/ui/layout";
 import { NotFound } from "~/ui/not-found";
 import { I18nProvider } from "~/i18n/i18n-context";
 import { Title } from "@solidjs/meta";
+import { useThemeListener } from "@kobalte/solidbase/client";
 
 export default function (props: RouteSectionProps) {
+	useThemeListener();
+
 	return (
 		<I18nProvider>
 			<Title>Solid Docs</Title>

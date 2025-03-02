4a51276b107f09fac22cf35be7fc3ef7da15fe20
jer3m01 <jer3m01@jer3m01.com>
2025-03-03 00:41:15 +0100

    fix: theme selector

--- src/solidbase-theme/Layout.tsx
+++ src/solidbase-theme/Layout.tsx
@@ -6,9 +6,11 @@ import { NotFound } from "~/ui/not-found";
 import { I18nProvider } from "~/i18n/i18n-context";
 import { Title } from "@solidjs/meta";
 import { useThemeListener } from "@kobalte/solidbase/client";
+import { usePace } from "@kobalte/solidbase/default-theme/pace.js";
 
 export default function (props: RouteSectionProps) {
 	useThemeListener();
+	usePace();
 
 	return (
 		<I18nProvider>
--- src/ui/layout/theme-selector.tsx
+++ src/ui/layout/theme-selector.tsx
@@ -1,6 +1,6 @@
-import { Component, For, JSX, Show } from "solid-js";
+import { children, Component, ComponentProps, createSignal, JSX, onMount, Show } from "solid-js";
 import { computerDesktop, moon, sun } from "solid-heroicons/solid";
-import { DropdownMenu } from "@kobalte/core";
+import { Select } from "@kobalte/core/select";
 import {
 	getThemeVariant,
 	setTheme,
@@ -37,60 +37,70 @@ export const ThemeSelector: Component = () => {
 		THEME_OPTIONS.find((t) => t.value === getThemeVariant());
 
 	return (
-		<DropdownMenu.Root gutter={10}>
-			<DropdownMenu.Trigger class="flex h-6 w-6 items-center justify-center rounded-lg shadow-md shadow-black/5 ring-1 ring-black/10 dark:bg-slate-800 dark:ring-inset dark:ring-white/60 shrink-0">
-				<Show when={selectedTheme()} fallback={<></>} keyed>
-					{(theme) => {
-						return (
+		<Select<ThemeOption>
+			options={THEME_OPTIONS}
+			optionValue="value"
+			optionTextValue="label"
+			value={selectedTheme()}
+			onChange={(option) => {
+				setTheme(option?.value);
+			}}
+			allowDuplicateSelectionEvents
+			gutter={10}
+			sameWidth={false}
+			placement="bottom-end"
+			itemComponent={(props) => (
+				<Select.Item
+					class="flex cursor-pointer select-none items-center rounded-[0.625rem] p-1 hover:bg-slate-200 hover:dark:bg-slate-600 group kb-selected:bg-slate-200 kb-selected:dark:bg-slate-700 kb-selected:font-semibold outline-none"
+					item={props.item}
+				>
+					<Select.ItemLabel>
+						<Icon
+							class="inline-block h-6 w-6 mr-2 rounded-md bg-white p-1 shadow ring-1 ring-slate-900/5 dark:bg-slate-700 dark:ring-inset dark:ring-white/5 fill-slate-700 dark:fill-slate-200 kb-selected:fill-slate-800 kb-selected:dark:fill-white"
+							path={props.item.rawValue.icon}
+						/>
+						<span
+							class="prose prose-slate text-sm group-hover:dark:text-white text-slate-700 dark:text-slate-300 kb-selected:text-blue-500 kb-selected:group-hover:dark:text-white"
+						>
+							{props.item.rawValue.label}
+						</span>
+					</Select.ItemLabel>
+				</Select.Item>
+			)}
+		>
+			<Select.Trigger class="flex h-6 w-6 items-center justify-center rounded-lg shadow-md shadow-black/5 ring-1 ring-black/10 dark:bg-slate-800 dark:ring-inset dark:ring-white/60 shrink-0" aria-label="Change theme mode">
+				<Select.Value<ThemeOption>>
+					{(state) => (
+						<RefreshOnMount aria-label={state.selectedOption().label}>
 							<Icon
 								class="w-4 h-4 fill-slate-700 dark:fill-slate-200"
-								path={theme.icon}
+								path={state.selectedOption().icon}
 							/>
-						);
-					}}
-				</Show>
-			</DropdownMenu.Trigger>
-			<DropdownMenu.Portal>
-				<DropdownMenu.Content class="z-50 w-36 space-y-1 rounded-xl bg-white p-2 text-sm shadow-md shadow-black/5 ring-1 ring-black/5 dark:bg-slate-800 dark:ring-white/5">
-					<For each={THEME_OPTIONS}>
-						{(theme) => (
-							<DropdownMenu.Item
-								class="flex cursor-pointer select-none items-center rounded-[0.625rem] p-1 hover:bg-slate-200 hover:dark:bg-slate-600 group"
-								classList={{
-									"bg-slate-200 dark:bg-slate-700 font-semibold":
-										selectedTheme()!.value === theme.value,
-								}}
-								closeOnSelect={true}
-								onSelect={() => {
-									setTheme(theme.value);
-								}}
-							>
-								<Icon
-									class="h-6 w-6 mr-2 rounded-md bg-white p-1 shadow ring-1 ring-slate-900/5 dark:bg-slate-700 dark:ring-inset dark:ring-white/5"
-									classList={{
-										"fill-slate-800 dark:fill-white":
-											selectedTheme()!.value === theme.value,
-										"fill-slate-700 dark:fill-slate-200":
-											selectedTheme()!.value !== theme.value,
-									}}
-									path={theme.icon}
-								/>
-								<span
-									class="prose prose-slate text-sm group-hover:dark:text-white"
-									classList={{
-										"text-blue-500 group-hover:dark:text-white":
-											selectedTheme()!.value === theme.value,
-										"text-slate-700 dark:text-slate-300":
-											selectedTheme()!.value !== theme.value,
-									}}
-								>
-									{theme.label}
-								</span>
-							</DropdownMenu.Item>
-						)}
-					</For>
-				</DropdownMenu.Content>
-			</DropdownMenu.Portal>
-		</DropdownMenu.Root>
+						</RefreshOnMount>
+					)}
+				</Select.Value>
+			</Select.Trigger>
+			<Select.Portal>
+				<Select.Content class="z-50 w-36 space-y-1 rounded-xl bg-white p-2 text-sm shadow-md shadow-black/5 ring-1 ring-black/5 dark:bg-slate-800 dark:ring-white/5">
+					<Select.Listbox />
+				</Select.Content>
+			</Select.Portal>
+		</Select>
 	);
 };
+
+function RefreshOnMount(props: ComponentProps<"div">) {
+	const resolved = children(() => props.children);
+
+	// incorrect value on server with no runtime, refresh on mount to update possibly incorrect label
+	const [refresh, setRefresh] = createSignal(false);
+	onMount(() => {
+		setRefresh(true);
+	});
+
+	return (
+		<Show when={refresh()} fallback={<div {...props}>{resolved()}</div>} keyed>
+			<div {...props}>{resolved()}</div>
+		</Show>
+	);
+}

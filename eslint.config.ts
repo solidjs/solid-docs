5e5566e07bd713f905c237274dd58fc310c5dc25
LadyBluenotes <gerrardsarah@gmail.com>
2025-06-13 14:15:07 -0700

    remove commented out code and mdx

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,6 @@
 import eslint from "@eslint/js";
 import solid from "eslint-plugin-solid";
 import * as globals from "globals";
-import * as mdx from "eslint-plugin-mdx";
 import tseslint from "typescript-eslint";
 import tsParser from "@typescript-eslint/parser";
 
@@ -54,82 +53,4 @@ export default [
 			],
 		},
 	},
-	{
-		...mdx.flat,
-		files: ["**/*.mdx"],
-		languageOptions: {
-			...mdx.flat.languageOptions,
-		},
-		rules: {
-			"@typescript-eslint/no-unused-expressions": "off",
-		},
-	},
-	{
-		...mdx.flatCodeBlocks,
-		rules: {
-			...mdx.flatCodeBlocks.rules,
-		},
-	},
 ];
-
-// 	tseslint.config(
-// 	{
-// 		name: "global-ignores",
-// 		ignores: [
-// 			"**/build/**",
-// 			"**/dist/**",
-// 			"**/node_modules/**",
-// 			".github/",
-// 			".solid/",
-// 			"public/",
-// 		],
-// 	},
-// 	eslint.configs.recommended,
-// 	{
-// 		files: ["**/*.{ts,tsx}"],
-// 		languageOptions: {
-// 			parser: tsParser,
-// 			parserOptions: {
-// 				project: "tsconfig.json",
-// 				ecmaVersion: "latest",
-// 				sourceType: "module",
-// 			},
-// 			globals: {
-// 				...globals.browser,
-// 				...globals.node,
-// 			},
-// 		},
-// 		plugins: {
-// 			solid,
-// 		},
-// 		rules: {
-// 			quotes: ["error", "double"],
-// 			semi: "warn",
-// 			"@typescript-eslint/no-unused-vars": [
-// 				"error",
-// 				{
-// 					args: "all",
-// 					argsIgnorePattern: "^_",
-// 					caughtErrors: "all",
-// 					caughtErrorsIgnorePattern: "^_",
-// 					destructuredArrayIgnorePattern: "^_",
-// 					varsIgnorePattern: "^_",
-// 					ignoreRestSiblings: true,
-// 				},
-// 			],
-// 		},
-// 	},
-// 	{
-// 		...mdx.flat,
-// 		files: ["**/*.mdx"],
-// 		languageOptions: {
-// 			...mdx.flat.languageOptions,
-// 		},
-// 	},
-// 	{
-// 		...mdx.flatCodeBlocks,
-// 		rules: {
-// 			...mdx.flatCodeBlocks.rules,
-// 		},
-// 	}
-// );
